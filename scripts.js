412d5eae96abc52007db29d2e4d37d917537d243
Matt <mattschock392@gmail.com>
2022-08-17 17:50:08 -0500

    working

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 let customerList = [];
 const $customer_Table = $('#tableappend');
+let id = 0
 
 class Customer {
-    constructor(customerName, phoneNumber, customerDOB,) {
+    constructor(customerName, phoneNumber, customerDOB, Id) {
         this.customerName = customerName;
         this.phoneNumber = phoneNumber;
         this.customerDOB = customerDOB;
+        this.id = id;
     }
 }
 
 function createCustomer() {
-    const addedCustomer = new Customer($(`#customer_name`).val(), $(`#customer_phone`).val(), $(`#customer_birthdate`).val())
+    const addedCustomer = new Customer($(`#customer_name`).val(), $(`#customer_phone`).val(), $(`#customer_birthdate`).val(), id)
     customerList.push(addedCustomer);
+    id++
     };
 
 
@@ -31,15 +34,30 @@ function renderTable() {
         <td>${customerList[i].customerDOB}
         </td>')
             <td>
-            <button Class="btn btn-danger" text="Delete">Delete</button>
+            <button class="btn btn-danger" text="Delete" onClick="deleteCustomer(${customerList[i].id})">Delete</button>
+            </td>
+            <td>
+            <button class="btn btn-primary" text="Delete" onClick="updateCustomer(${customerList[i].id})">Update</button>
             </td>
         </tr>')`
         
     )
     }
+    console.log(customerList);
 }
 
 
+function deleteCustomer(id) {
+    const indexToDelete = customerList.findIndex(customer => customer.id===id)
+    customerList.splice(indexToDelete, 1);
+    renderTable();
+}
 
+function updateCustomer(id) {
+    const indexToUpdate = customerList.findIndex(customer => customer.id ===id)
+    customerList[indexToUpdate].customerName =$('#customer_name').val();
+    customerList[indexToUpdate].phoneNumber = $('#customer_phone').val();
+    customerList[indexToUpdate].customerDOB = $('#customer_birthdate').val();
+    renderTable();
+}
 
-console.log(customerList);
