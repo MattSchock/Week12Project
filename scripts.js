e5011c06688dddf0d506bef8dcf39f9f1445741c
Matt <mattschock392@gmail.com>
2022-08-17 16:54:53 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -1,101 +1,45 @@
+let customerList = [];
+const $customer_Table = $('#tableappend');
+
 class Customer {
-    constructor(customerName, phoneNumber, customerDOB) {
+    constructor(customerName, phoneNumber, customerDOB,) {
         this.customerName = customerName;
         this.phoneNumber = phoneNumber;
         this.customerDOB = customerDOB;
     }
 }
 
-class CustomerInteraction {
+function createCustomer() {
+    const addedCustomer = new Customer($(`#customer_name`).val(), $(`#customer_phone`).val(), $(`#customer_birthdate`).val())
+    customerList.push(addedCustomer);
+    };
 
-    static url = "fd023aa6e639420db2a0d89deb0603c3/customers";
 
-    static showAllCustomers() {
-        return $.get(this.url);
-    }
 
-    static specificHouse(id) {
-        return $.get(this.url + `/${id}`);
-    }
 
-    static createCustomer(customer) {
-        return $.post(this.url, customer);
-    }
 
-    static updateCustomer(customer) {
-        return $.ajax({
-            url: this.url + `/${customer._id}`,
-            dataType: 'json',
-            data: JSON.stringify(customer),
-            contentType: 'application/json',
-            type: 'PUT'
-        });
-    }
+function renderTable() {
+    $customer_Table.empty();
 
-    static removeCustomer(id) {
-        return $.ajax({
-            url: this.url + `/${id}`,
-            type: 'DELETE'
-        });
+    for (let i = 0; i < customerList.length; i++) {
+    $customer_Table.append(`
+        $('<tr>')
+        $('<td>${customerList[i].customerName}
+        </td>
+        <td>${customerList[i].phoneNumber}
+        </td>
+        <td>${customerList[i].customerDOB}
+        </td>')
+            <td>
+            <button Class="btn btn-danger" text="Delete">Delete</button>
+            </td>
+        </tr>')`
+        
+    )
     }
 }
 
 
-class DOMManager {
-    static customers;
-
-    static showAllCustomers() {
-        CustomerInteraction.showAllCustomers().then(customers => this.render(customers));
-    }
-
-    static createCustomer(name) {
-        CustomerInteraction.createCustomer(new Customer(name))
-        .then(() => {
-            return CustomerInteraction.showAllCustomers();
-        })
-        .then((customers) => this.render(customers));
-    }
 
 
-    static removeCustomer(id) {
-        CustomerInteraction.removeCustomer(id)
-        .then(() => {
-            return CustomerInteraction.showAllCustomers();
-        })
-        .then((customers) => this.render(customers));
-    }
-
-
-    static render(customers) {
-        this.customers = customers;
-        $('#customer_table').empty();
-        for (let customer of customers) {
-            $('#customer_table').prepend(
-                `<div id="#{customomer._id}" class="card">
-                    <div class="card-header"
-                    <h3>${customer.name}</h3>
-                    <button class="btn btn-danger" onClick='DOMManager.deleteCustomer('${customer.id}')">Delete</button>
-                    </div>
-                </div>`
-            );
-        }
-
-
-
-
-
-
-    }
-
-
-
-}
-
-
-$('#create_customer').click(() => {
-    DOMManager.createCustomer($('#create_customer').val());
-    $('customer_name').val("");
-})
-
-DOMManager.showAllCustomers();
-
+console.log(customerList);
