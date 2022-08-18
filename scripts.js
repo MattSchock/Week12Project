4ef7dc8ddc01a6ae1d7b126a3f1bf244a2d0d2ae
Matt <mattschock392@gmail.com>
2022-08-18 13:00:44 -0500

    working

--- scripts.js
+++ scripts.js
@@ -1,8 +1,35 @@
+
+let = $myModal = $('#myModal');
 let customerList = [];
 const $customer_Table = $('#tableappend');
-let id = 0
+let id = 1
+const apiUrl = "https://62fd71deb9e38585cd51f570.mockapi.io/customers"
+
+
+function emptyApiArray() {
+    if (customerList.length == 0) {
+        id = 1;
+        return id;
+    }
+}
+
+
+
+
+
+const updateCustomerList = async () => {
+    let customerList = [];
+    const response = await fetch("https://62fd71deb9e38585cd51f570.mockapi.io/customers")
+    const data = await response.json();
+    customerList = data;
+    console.log(customerList);
+    return customerList;
+}
+
+
 
 class Customer {
+    
     constructor(customerName, phoneNumber, customerDOB, Id) {
         this.customerName = customerName;
         this.phoneNumber = phoneNumber;
@@ -12,7 +39,8 @@ class Customer {
 }
 
 function createCustomer() {
-    const addedCustomer = new Customer($(`#customer_name`).val(), $(`#customer_phone`).val(), $(`#customer_birthdate`).val(), id)
+    const addedCustomer = new Customer($(`#customer_name`).val(), $(`#customer_phone`).val(), $(`#customer_birthdate`).val(), id);
+    $.post(apiUrl, addedCustomer);
     customerList.push(addedCustomer);
     id++
     };
@@ -23,9 +51,8 @@ function createCustomer() {
 
 function renderTable() {
     $customer_Table.empty();
-    
+    updateCustomerList();
     for (let i = 0; i < customerList.length; i++) {
-        let passedValue = `${customerList[i].id}`;
     $customer_Table.append(`
         $('<tr>')
         $('<td>${customerList[i].customerName}
@@ -38,13 +65,12 @@ function renderTable() {
             <button class="btn btn-danger" text="Delete" onClick="deleteCustomer(${customerList[i].id})">Delete</button>
             </td>
             <td>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-modal" >
+            <button type="button" class="btn btn-primary" data-bs-target="#edit-modal" onClick="updateCustomer(${customerList[i].id})" >
             Edit
             </button>
             </td>
         </tr>')`
     )
-    return passedValue
     }
     console.log(customerList);
 }
@@ -52,22 +78,93 @@ function renderTable() {
 
 function deleteCustomer(id) {
     const indexToDelete = customerList.findIndex(customer => customer.id===id)
+    $.ajax({
+        url: apiUrl + `/${id}` ,
+        type: 'DELETE'
+    });
+
     customerList.splice(indexToDelete, 1);
     renderTable();
+    emptyApiArray();
 }
 
 
 
 function updateCustomer(id) {
     const indexToUpdate = customerList.findIndex(customer => customer.id ===id)
-    customerList[indexToUpdate].customerName = $('#name-edit').val();
-    customerList[indexToUpdate].phoneNumber = $('#tel-edit').val();
-    customerList[indexToUpdate].customerDOB = $('#DOB-edit').val();
+    customerList[indexToUpdate].customerName = $('#customer_name').val();
+    customerList[indexToUpdate].phoneNumber = $('#customer_phone').val();
+    customerList[indexToUpdate].customerDOB = $('#customer_birthdate').val();
+    $.ajax({
+        url: apiUrl + `/${id}`,
+        type: 'PUT',
+        data: {'customerName': $('#customer_name').val(),
+                'phoneNumber': $('#customer_phone').val(),
+                'customerDOB': $('#customer_birthdate').val()  }
+
+    })
     renderTable();
     console.log(customerList);
 }
 
-function passIdValue() {
-    let passedValue = $(customerList[i].id);
-    return passedValue;
-}
+
+
+
+// function showModal(id) {
+//     $myModal.append(`
+//     <div class="modal" tabindex="-1" id="edit-modal">
+//     <div class="modal-dialog">
+//       <div class="modal-content">
+//         <div class="modal-header">
+//           <h5 class="modal-title">Modal title</h5>
+//           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//         </div>
+//         <div class="modal-body">
+//             <label for="name-edit" class="form-label">Name</label>
+//             <input type="text" class="form-control" id="name-edit">
+//             <label for="tel-edit" class="form-label">Phone</label>
+//             <input type="tel" class="form-control" id="tel-edit">
+//             <label for="DOB-edit" class="form-label">DOB</label>
+//             <input type="date" class="form-control" id="DOB-edit">
+//         </div>
+//         <div class="modal-footer">
+//           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//           <button type="button" class="btn btn-primary" onClick="updateCustomer()">Save changes</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   `)
+
+//     console.log('dynamic id', id)
+//     console.log(`<div class="modal" tabindex="-1" id="edit-modal">
+//     <div class="modal-dialog">
+//       <div class="modal-content">
+//         <div class="modal-header">
+//           <h5 class="modal-title">Modal title</h5>
+//           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//         </div>
+//         <div class="modal-body">
+//             <label for="name-edit" class="form-label">Name</label>
+//             <input type="text" class="form-control" id="name-edit">
+//             <label for="tel-edit" class="form-label">Phone</label>
+//             <input type="tel" class="form-control" id="tel-edit">
+//             <label for="DOB-edit" class="form-label">DOB</label>
+//             <input type="date" class="form-control" id="DOB-edit">
+//         </div>
+//         <div class="modal-footer">
+//           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//           <button type="button" class="btn btn-primary" onClick="updateCustomer()">Save changes</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>`)
+// }
+
+
+
+
+// function passIdValue() {
+//     let passedValue = $(customerList[i].id);
+//     return passedValue;
+// }
--- trash.js
+++ trash.js
@@ -1,116 +1,116 @@
-class Customer {
-    constructor(customerName, phoneNumber, customerDOB) {
-        this.customerName = customerName;
-        this.phoneNumber = phoneNumber;
-        this.customerDOB = customerDOB;
-        this.customers = []
-    }
-}
+// class Customer {
+//     constructor(customerName, phoneNumber, customerDOB) {
+//         this.customerName = customerName;
+//         this.phoneNumber = phoneNumber;
+//         this.customerDOB = customerDOB;
+//         this.customers = []
+//     }
+// }
 
-class CustomerInteraction {
+// class CustomerInteraction {
 
-    static url = "https://crudcrud.com/api/fd023aa6e639420db2a0d89deb0603c3/customers";
+//     static url = "https://crudcrud.com/api/fd023aa6e639420db2a0d89deb0603c3/customers";
 
-    static showAllCustomers = async () => {
-        const response = await fetch("https://crudcrud.com/api/fd023aa6e639420db2a0d89deb0603c3/customers")
-        const data = await response.json();
-        // Customer.customers.push(new Customer($(`${customer._id}-`)));
-    }
+//     static showAllCustomers = async () => {
+//         const response = await fetch("https://crudcrud.com/api/fd023aa6e639420db2a0d89deb0603c3/customers")
+//         const data = await response.json();
+//         // Customer.customers.push(new Customer($(`${customer._id}-`)));
+//     }
     
 
-    static specificHouse(id) {
-        return $.get(this.url + `/${id}`);
-    }
+//     static specificHouse(id) {
+//         return $.get(this.url + `/${id}`);
+//     }
 
-    static createCustomer(customer) {
-        fetch('https://crudcrud.com/api/fd023aa6e639420db2a0d89deb0603c3/customers', {
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            method: 'POST',
-            body: JSON.stringify({
-            name: 'Write Tests',
-            done: false
-        })
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-}
-
-
-
-    static updateCustomer(customer) {
-        return $.ajax({
-            url: this.url + `/${customer._id}`,
-            dataType: 'json',
-            data: JSON.stringify(customer),
-            contentType: 'application/json',
-            type: 'PUT'
-        });
-    }
-
-    static removeCustomer(id) {
-        return $.ajax({
-            url: this.url + `/${id}`,
-            type: 'DELETE'
-        });
-    }
-}
+//     static createCustomer(customer) {
+//         fetch('https://crudcrud.com/api/fd023aa6e639420db2a0d89deb0603c3/customers', {
+//             headers: { "Content-Type": "application/json; charset=utf-8" },
+//             method: 'POST',
+//             body: JSON.stringify({
+//             name: 'Write Tests',
+//             done: false
+//         })
+//     })
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+// }
 
 
-class DOMManager {
-    static customers;
 
-    static showAllCustomers() {
-        CustomerInteraction.showAllCustomers().then(customers => this.render(customers));
-    }
+//     static updateCustomer(customer) {
+//         return $.ajax({
+//             url: this.url + `/${customer._id}`,
+//             dataType: 'json',
+//             data: JSON.stringify(customer),
+//             contentType: 'application/json',
+//             type: 'PUT'
+//         });
+//     }
 
-    static createCustomer(name) {
-        CustomerInteraction.createCustomer(new Customer(name))
-        .then(() => {
-            return CustomerInteraction.showAllCustomers();
-        })
-        .then((customers) => this.render(customers));
-    }
-
-
-    static removeCustomer(id) {
-        CustomerInteraction.removeCustomer(id)
-        .then(() => {
-            return CustomerInteraction.showAllCustomers();
-        })
-        .then((customers) => this.render(customers));
-    }
+//     static removeCustomer(id) {
+//         return $.ajax({
+//             url: this.url + `/${id}`,
+//             type: 'DELETE'
+//         });
+//     }
+// }
 
 
-     static render(customers) {
-        this.customers = customers;
-        $('#customer_table').empty();
-        for (let customer of customers) {
-            $('#customer_table').prepend(
-                `<div id="#{customomer._id}" class="card">
-                    <div class="card-header"
-                    <h3>${customer.name}</h3>
-                    <button class="btn btn-danger" onClick='DOMManager.deleteCustomer('${customer.id}')">Delete</button>
-                   </div>
-                </div>`
-           );
-         }
+// class DOMManager {
+//     static customers;
+
+//     static showAllCustomers() {
+//         CustomerInteraction.showAllCustomers().then(customers => this.render(customers));
+//     }
+
+//     static createCustomer(name) {
+//         CustomerInteraction.createCustomer(new Customer(name))
+//         .then(() => {
+//             return CustomerInteraction.showAllCustomers();
+//         })
+//         .then((customers) => this.render(customers));
+//     }
 
 
+//     static removeCustomer(id) {
+//         CustomerInteraction.removeCustomer(id)
+//         .then(() => {
+//             return CustomerInteraction.showAllCustomers();
+//         })
+//         .then((customers) => this.render(customers));
+//     }
+
+
+//      static render(customers) {
+//         this.customers = customers;
+//         $('#customer_table').empty();
+//         for (let customer of customers) {
+//             $('#customer_table').prepend(
+//                 `<div id="#{customomer._id}" class="card">
+//                     <div class="card-header"
+//                     <h3>${customer.name}</h3>
+//                     <button class="btn btn-danger" onClick='DOMManager.deleteCustomer('${customer.id}')">Delete</button>
+//                    </div>
+//                 </div>`
+//            );
+//          }
 
 
 
 
-    }
+
+
+//     }
 
 
 
-}
+// }
 
 
-$('#create_customer').click(() => {
-    DOMManager.createCustomer($('#create_customer').val());
-    $('customer_name').val("");
-})
+// $('#create_customer').click(() => {
+//     DOMManager.createCustomer($('#create_customer').val());
+//     $('customer_name').val("");
+// })
 
-DOMManager.showAllCustomers();
+// DOMManager.showAllCustomers();
 
